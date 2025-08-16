e86a27466ed64e344497a31bc5aa6e54f50f1821
Macaaarony <jonasvincemaca@gmail.com>
2025-08-16 21:33:30 +0800

    Update 2-setup.sh

--- 2-setup.sh
+++ 2-setup.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-# Update User Directories (Documents,Downloads,...)
+# Update User Directories (Documents, Downloads, etc.)
 xdg-user-dirs-update
 
-# Install Neovim
-cd
-git clone https://github.com/neovim/neovim
-cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
-git checkout stable
-cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb
-
-# Installing i3 gaps
-cd
-cd Downloads
+# --- Installing i3-gaps ---
+cd ~/Downloads
 git clone https://github.com/Airblader/i3 i3-gaps
 cd i3-gaps
 mkdir -p build && cd build
@@ -20,19 +12,7 @@ meson --prefix /usr/local
 ninja
 sudo ninja install
 
-# Installing dependencies for Ly Console Manager
-sudo nala install -y libpam0g-dev libxcb-xkb-dev
-
-# Installing Ly Console Display Manager
-cd
-cd Downloads
-git clone --recurse-submodules https://github.com/fairyglade/ly
-cd ly/
-make
-sudo make install installsystemd
-sudo systemctl enable ly.service
-
-# Configure xsessions
+# --- Configure xsessions (so i3 shows up in LightDM session list) ---
 if [[ ! -d /usr/share/xsessions ]]; then
     sudo mkdir /usr/share/xsessions
 fi
@@ -41,9 +21,11 @@ cat > ./temp << "EOF"
 [Desktop Entry]
 Encoding=UTF-8
 Name=i3
-Comment=Manual Window Manager
+Comment=Tiling Window Manager (i3-gaps)
 Exec=i3
 Icon=i3
 Type=XSession
 EOF
-sudo cp ./temp /usr/share/xsessions/i3.desktop;rm ./temp
+
+sudo cp ./temp /usr/share/xsessions/i3.desktop
+rm ./temp
